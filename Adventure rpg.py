1f03433dbfb7047bef843f362d787e3f873095b1
timiodu <eto26@kent.ac.uk>
2026-03-19 21:48:49 +0000

    i have defined the player stats clearly shown in the player section giving a name to each system that will be used in the rpg

--- Adventure rpg.py
+++ Adventure rpg.py
@@ -1,2 +1,15 @@
 import random
 import re
+
+#       PLAYER
+player = {
+    "hp": 100,
+    "max_hp": 100,
+    "energy": 5,
+    "max_energy": 7
+}
+
+#       CARDS
+cards = [
+    {"name": "Strike", "damage":
+]
