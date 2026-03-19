bd623ac1445a828420a70212efcb125e571dec35
timiodu <eto26@kent.ac.uk>
2026-03-19 21:53:15 +0000

    As for the card skills i have made a library of the avaliable skills that can be used and that can actually be dropped, so regarding the random import it will be used as part for events where after defeating an enemy the adventurer can obtain a new skill

--- Adventure rpg.py
+++ Adventure rpg.py
@@ -11,5 +11,10 @@ player = {
 
 #       CARDS
 cards = [
-    {"name": "Strike", "damage":
+    {"name": "Strike", "damage": 10, "cost": 2}
+    {"name": "Quick Slash", "damage": 12, "cost": 2}
+    {"name": "Venom Slash", "damage": 18, "cost": 3}
+    {"name": "Thousand Myriad Blades", "damage": 35, "cost": 4}
 ]
+
+
