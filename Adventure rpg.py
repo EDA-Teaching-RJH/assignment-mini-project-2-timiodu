ab67d2d6be2f27989fb3f28c0da6834b8138be78
timiodu <eto26@kent.ac.uk>
2026-03-24 21:08:12 +0000

    Have added the enemy library now, they will be possibly used multiples times within the actual gameplay, used same format as cards as shown in code

--- Adventure rpg.py
+++ Adventure rpg.py
@@ -8,7 +8,6 @@ player = {
     "energy": 5,
     "max_energy": 7
 }
-
 #       CARDS
 cards = [
     {"name": "Strike", "damage": 10, "cost": 2},
@@ -16,5 +15,11 @@ cards = [
     {"name": "Venom Slash", "damage": 18, "cost": 3},
     {"name": "Thousand Myriad Blades", "damage": 35, "cost": 4}
 ]
-
-
+#       ENEMIES!
+enemies = [
+    {"name": "Bandit Scout", "hp": 45, "damage": 8, "elite": False }
+    {"name": "Controlled Wolf", "hp": 35, "damage": 9, "elite: False}
+    {"name": "Bandit Scout", "hp": 90, "damage": 14, "elite": True}
+]
+#       FOR THE FUNCTIONS!
+def intro():
