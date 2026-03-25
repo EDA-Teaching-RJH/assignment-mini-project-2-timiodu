c02017be445fa4c757b58176e3db3ba7076c1e12
timiodu <eto26@kent.ac.uk>
2026-03-25 00:47:23 +0000

    This new code written divides into sections, which consist of multiple functions, or pieces of code that complete particular tasks, such as displaying energy, drawing cards, and inspecting enemies. to help with organization and readability. my game also randomly selects what happens, which helps to reduce predictability in how the game will proceed. now the program stores data such as player statistics and card details in a structured format so that the data from the libraries can easily be accessed. It does consist of small but memorable and effective codes such as len, random and \n as these have been used in the past

--- Adventure rpg.py
+++ Adventure rpg.py
@@ -1,14 +1,14 @@
 import random
 import re
 
-#       PLAYER
+#       PLAYER!
 player = {
     "hp": 100,
     "max_hp": 100,
     "energy": 5,
     "max_energy": 7
 }
-#       CARDS
+#       CARDS!
 cards = [
     {"name": "Strike", "damage": 10, "cost": 2},
     {"name": "Quick Slash", "damage": 12, "cost": 2},
@@ -17,9 +17,28 @@ cards = [
 ]
 #       ENEMIES!
 enemies = [
-    {"name": "Bandit Scout", "hp": 45, "damage": 8, "elite": False }
-    {"name": "Controlled Wolf", "hp": 35, "damage": 9, "elite: False}
+    {"name": "Bandit Scout", "hp": 45, "damage": 8, "elite": False },
+    {"name": "Controlled Wolf", "hp": 35, "damage": 9, "elite": False},
     {"name": "Bandit Scout", "hp": 90, "damage": 14, "elite": True}
 ]
 #       FOR THE FUNCTIONS!
 def intro():
+    print("You, a hero, is camping in a peaceful Village...")
+    print("Your heightened instincts start to activate out of nowhere, you feel uneasy.")
+    print("Bandits rush towards the village, with somewhat obidient and controlled monsters???\n")
+def show_energy():
+    print("Energy:", "⚡️" * player["energy"], f"({player['energy']}/7)")
+def draw_cards():
+    return random.sample(cards,3)
+def inspect_enemy(enemy):
+    print("\nEnemy:", enemy["name"])
+    print("HP", enemy["hp"])
+    print("Damage", enemy["damage"])
+def inspect_card(hand, index):
+    if 0 <= index < len(hand):
+        card = hand[index]
+        print(f"\n{card['name']}:")
+        print("Damage:", card["damage"])
+        print("Cost:", card["cost"])
+
+
