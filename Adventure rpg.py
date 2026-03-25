07db00e580e2193c9bf685471a2c52886b03fbdc
timiodu <eto26@kent.ac.uk>
2026-03-25 22:20:36 +0000

    Here, regular expressions aid the game in comprehending a variety of player commands, such as "use 1" or "inspect enemy." The system looks for these patterns, determines exactly what the player wants to do, and then executes it using if/elif statements. Additionally, it takes care with input, so if someone types something incorrectly or selects a card that doesn't exist, the game won't crash; instead, it will simply notify you and continue. Instead of being limited to dull number-only inputs, this method makes the entire command system feel much more dynamic and user-friendly.

--- Adventure rpg.py
+++ Adventure rpg.py
@@ -19,7 +19,7 @@ cards = [
 enemies = [
     {"name": "Bandit Scout", "hp": 45, "damage": 8, "elite": False },
     {"name": "Controlled Wolf", "hp": 35, "damage": 9, "elite": False},
-    {"name": "Bandit Scout", "hp": 90, "damage": 14, "elite": True}
+    {"name": "Elite ", "hp": 90, "damage": 14, "elite": True}
 ]
 #       FOR THE FUNCTIONS!
 def intro():
@@ -40,5 +40,43 @@ def inspect_card(hand, index):
         print(f"\n{card['name']}:")
         print("Damage:", card["damage"])
         print("Cost:", card["cost"])
+def player_turn(enemy):
+    hand = draw_cards()
+    print("\nYour cards:")
+    for i, card in enumerate(hand):
+        print(f"{i+1}. {card['name']} (Cost {card['cost']})")
+    while True:
+        command = input("\nCommand: ").lower()
+
+#               FOR THE USES OF CARDS ONE AFTER ANOTHER!!
+        match = re.match(r"(use|u)\s*(\d+)", command)
+        if match:
+            choice = int(match.group(2)) -1
+            if 0 <= choice < len(hand):
+                card = hand[choice]
+
+                if player["energy"] >= card["cost"]:
+                    player["energy"] -= card["cost"]
+                    enemy["hp"] -= card["damage"]
+                    print(f"You used {card['name']} for {card['damage']} damage!!!")
+                    return
+                else: print("Not enough energy!")
+            else: 
+                print("Invalid Card Number!!")
+#               If u wanna inspect ur enemy!
+        elif re.match(r"inspect enemy", command):
+            inspect_enemy(hand, idx)
+#               Inspect ur cards!
+        elif re.match(r"inspect (\d+)", command):
+            idx = int(re.match(r"inspect (\d+)", command).group(1)) - 1
+            inspect_card(hand, idx)
+
+        # skip turn
+        elif re.match(r"skip", command):
+            print("You skipped your turn.")
+            return
+
+        else:
+            print("Invalid command.")
 
 
